515bfc6017f47cd004cf0b2e5d4e4c7ed5083519
james <jamesomosora@gmail.com>
2018-05-25 14:58:24 +0800

    james omosora coverage

--- resources/views/carewell/pages/settings_coverage_plan.blade.php
+++ resources/views/carewell/pages/settings_coverage_plan.blade.php
@@ -37,7 +37,7 @@
                 <th>PLAN ADDED</th>
                 <th>ACTION</th>
               </tr>
-              {{-- @foreach($_active_coverage_plan as $coverage_plan)
+              @foreach($_active_coverage_plan as $coverage_plan)
               <tr>
                 <td>{{$coverage_plan->coverage_plan_id}}</td>
                 <td>{{$coverage_plan->coverage_plan_name}}</td>
@@ -60,11 +60,11 @@
                   </div>
                 </td>
               </tr>
-              @endforeach --}}
+              @endforeach
             </table>
           </div>
           <div class="box-footer clearfix">
-            {{-- @include('globals.pagination', ['paginator' => $_active_coverage_plan]) --}}
+            @include('globals.pagination', ['paginator' => $_active_coverage_plan])
           </div>
         </div>
         <!-- /.tab-pane -->
@@ -92,15 +92,14 @@
                 <th>PLAN ADDED</th>
                 <th>ACTION</th>
               </tr>
-              {{-- @foreach($_inactive_coverage_plan as $coverage_plan)
+              @foreach($_inactive_coverage_plan as $coverage_plan)
               <tr>
                 <td>{{$coverage_plan->coverage_plan_id}}</td>
                 <td>{{$coverage_plan->coverage_plan_name}}</td>
-                <td>{{$coverage_plan->coverage_plan_maximum_benefit}}</td>
-                <td>{{$coverage_plan->coverage_plan_annual_benefit}}</td>
-                <td>{{$coverage_plan->coverage_plan_case_handling}}</td>
+                <td>{{number_format($coverage_plan->coverage_plan_maximum_benefit)}}</td>
+                <td>{{number_format($coverage_plan->coverage_plan_annual_benefit)}}</td>
                 <td>{{$coverage_plan->coverage_plan_preexisting}}</td>
-                <td>{{$coverage_plan->coverage_plan_premium}}</td>
+                <td>{{number_format($coverage_plan->coverage_plan_premium)}}</td>
                 <td>{{date("F j, Y",strtotime($coverage_plan->coverage_plan_created))}}</td>
                 <td>
                   <div class="btn-group">
@@ -116,11 +115,11 @@
                   </div>
                 </td>
               </tr>
-              @endforeach --}}
+              @endforeach
             </table>
           </div>
           <div class="box-footer clearfix">
-            {{-- @include('globals.pagination', ['paginator' => $_inactive_coverage_plan]) --}}
+            @include('globals.pagination', ['paginator' => $_inactive_coverage_plan])
           </div>
         </div>
       </div>
