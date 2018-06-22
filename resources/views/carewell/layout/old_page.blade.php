9bae9fba36c83a34283abe079ad025f1a37b1e42
james <jamesomosora@gmail.com>
2018-06-22 18:29:25 +0800

    james layout

---
+++ resources/views/carewell/layout/old_page.blade.php
@@ -0,0 +1,77 @@
+<li style="background-color:#913D88">
+                        <a href="/dashboard">
+                            <i class="fa fa-dashboard"></i> <span>DASHBOARD</span>
+                        </a>
+                    </li>
+                    <li class="treeview" style="background-color: #03A678">
+                        <a href="#">
+                            <i class="fa fa-cog"></i>
+                            <span>SETTINGS</span>
+                            <span class="pull-right-container">
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </span>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li style="background-color:#3FC380"><a href="/settings/admin"><i class="fa fa-user"></i>ADMIN PANEL</a></li>
+                            <li style="background-color:#3FC380"><a href="/settings/access"><i class="fa fa-universal-access"></i>ACCESS LEVEL</a></li>
+                            <li style="background-color:#03A678" ><a href="/settings/coverage"><i class="fa fa-circle-o"></i>COVERAGE PLAN</a></li>
+                            <li style="background-color:#03A678"><a href="/settings/maintenance"><i class="fa fa-circle-o"></i>MAINTENANCE</a></li>
+                            <li style="background-color:#049372">
+                                <a href="/provider">
+                                    <i class="fa fa-hospital-o"></i> <span style="font-size:.9em;">PROVIDER CENTER</span>
+                                </a>
+                            </li>
+                            <li style="background-color:#1E824C">
+                                <a href="/doctor">
+                                    <i class="fa fa-user-md"></i> <span style="font-size:.9em;">DOCTOR CENTER</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    
+                    <li style="background-color:#C0392B">
+                        <a href="/company">
+                            <i class="fa fa-building-o"></i> <span>COMPANY CENTER</span>
+                        </a>
+                    </li>
+                    <li style="background-color:#446CB3">
+                        <a href="/member">
+                            <i class="fa fa-user"></i> <span>MEMBER CENTER</span>
+                        </a>
+                    </li>
+                    <li style="background-color:#E87E04">
+                        <a href="/billing">
+                            <i class="fa fa-credit-card"></i> <span>BILLING CENTER</span>
+                        </a>
+                    </li>
+                    <li style="background-color:#336E7B">
+                        <a href="/availment">
+                            <i class="fa fa-medkit"></i> <span>AVAILMENT CENTER</span>
+                        </a>
+                    </li>
+                    <li style="background-color:#F64747">
+                        <a href="/payable">
+                            <i class="fa fa-paypal"></i> <span>PAYABLE</span>
+                        </a>
+                    </li>
+                    
+                    <li style="background-color:#1BBC9B" class="treeview" >
+                        <a href="#">
+                            <i class="fa fa-bar-chart"></i>
+                            <span>REPORTS</span>
+                            <span class="pull-right-container">
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </span>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li style="background-color:#26A65B"><a href="/reports/availment_per_month"><i class="fa fa-circle-o"></i>AVAILMENT</a></li>
+                            <li style="background-color:#26A65B"><a href="/reports/company_availment"><i class="fa fa-circle-o"></i><span style="font-size:12px;">COMPANY AVAILMENT</span></a></li>
+                            <li style="background-color:#26A65B"><a href="/reports/availment_monitoring"><i class="fa fa-circle-o"></i>MONITORING</a></li>
+                            <li style="background-color:#26A65B"><a href="/reports/payment_report"><i class="fa fa-circle-o"></i>PAYMENT REPORT</a></li>
+                            <li style="background-color:#26A65B"><a href="/reports/ending_number_per_month"><i class="fa fa-circle-o"></i>ENDING NUMBER</a></li>
+                            <li style="background-color:#26A65B"><a href="/reports/breakdown"><i class="fa fa-circle-o"></i>BREAKDOWN</a></li>
+                            <!-- <li style="background-color:#26A65B"><a href="/reports/consolidation"><i class="fa fa-circle-o"></i>CONSOLIDATION</a></li> -->
+                            <!-- <li style="background-color:#26A65B"><a href="#"><i class="fa fa-circle-o"></i>SUMMARY</a></li> -->
+                            <li style="background-color:#26A65B"><a href="/reports/active_per_month"><i class="fa fa-circle-o"></i><span style="font-size:12px;">ACTIVE PER MONTH</span></a></li>
+                        </ul>
+                    </li>
--- resources/views/carewell/layout/page.blade.php
+++ resources/views/carewell/layout/page.blade.php
@@ -2,43 +2,33 @@
 
 $_item = array(
 
-    "dasboard" => array('access_id'=>1,'text'=>'Dashboard',  'href'=>'/dashboard','style'=>"background-color:#913D88","class"=>"fa fa-dashboard"),
+    "dasboard"  => array('access_id'=>1,'text'=>'Dashboard',  'href'=>'/dashboard','style'=>"background-color:#913D88","class"=>"fa fa-dashboard"),
      "settings" => array(
-                    'admin_pannel'  => array('access_id'=>1,'text'=>'ADMIN PANEL',  'href'=>'/settings/admin','style'=>"background-color:#3FC380","class"=>"fa fa-user"),
-                    'access_level'  => array('access_id'=>1,'text'=>'CCESS LEVEL',  'href'=>'/settings/access','style'=>"background-color:#3FC380","class"=>"fa fa-universal-access"),
-                    'coverage_plan' => array('access_id'=>1,'text'=>'COVERAGE PLAN',  'href'=>'/settings/coverage','style'=>"background-color:#03A678","class"=>"fa fa-fa-circle-o"),
-                    'maintenance'   => array('access_id'=>1,'text'=>'AINTENANCE',  'href'=>'/settings/maintenance','style'=>"background-color:#03A678","class"=>"fa fa-fa-circle-o"),
+                    'admin_pannel'  => array('access_id'=>1,'text'=>'ADMIN PANEL',      'href'=>'/settings/admin','style'=>"background-color:#3FC380","class"=>"fa fa-user"),
+                    'access_level'  => array('access_id'=>1,'text'=>'CCESS LEVEL',      'href'=>'/settings/access','style'=>"background-color:#3FC380","class"=>"fa fa-universal-access"),
+                    'coverage_plan' => array('access_id'=>1,'text'=>'COVERAGE PLAN',    'href'=>'/settings/coverage','style'=>"background-color:#03A678","class"=>"fa fa-fa-circle-o"),
+                    'maintenance'   => array('access_id'=>1,'text'=>'AINTENANCE',       'href'=>'/settings/maintenance','style'=>"background-color:#03A678","class"=>"fa fa-fa-circle-o"),
                     'provider'      => array('access_id'=>1,'text'=>'PROVIDER CENTER',  'href'=>'/provider','style'=>"background-color:#049372","class"=>"fa fa-hospital-o"),
-                    'doctor'        => array('access_id'=>1,'text'=>'DOCTOR CENTER',  'href'=>'/doctor','style'=>"background-color:#1E824C","class"=>"fa fa-user-md"),
+                    'doctor'        => array('access_id'=>1,'text'=>'DOCTOR CENTER',    'href'=>'/doctor','style'=>"background-color:#1E824C","class"=>"fa fa-user-md"),
                 ),
-    "company" => array('access_id'=>1,'text'=>'COMPANY CENTER',  'href'=>'/company','style'=>"background-color:#C0392B","class"=>"fa fa-building-o"),
-    "member" => array('access_id'=>1,'text'=>'MEMBER CENTER',  'href'=>'/member','style'=>"background-color:#446CB3","class"=>"fa fa-fa-user"),
-    "billing" => array('access_id'=>1,'text'=>'Dashboard',  'href'=>'/billing','style'=>"background-color:#E87E04","class"=>"fa fa-credit-card"),
-    "availment" => array('access_id'=>1,'text'=>'Dashboard',  'href'=>'/availment','style'=>"background-color:#336E7B","class"=>"fa fa-medkit"),
-    "payable" => array('access_id'=>1,'text'=>'Dashboard',  'href'=>'/payable','style'=>"background-color:#F64747","class"=>"fa fa-paypal"),
-    "reports" => array(
-                    'admin_pannel'  => array('access_id'=>1,'text'=>'Dashboard',  'href'=>'/dashboard','style'=>"background-color:#913D88","class"=>"fa fa-dashboard"),
-                    'access_level'  => array('access_id'=>1,'text'=>'Dashboard',  'href'=>'/dashboard','style'=>"background-color:#913D88","class"=>"fa fa-dashboard"),
-                    'coverage_plan' => array('access_id'=>1,'text'=>'Dashboard',  'href'=>'/dashboard','style'=>"background-color:#913D88","class"=>"fa fa-dashboard"),
-                    'maintenance'   => array('access_id'=>1,'text'=>'Dashboard',  'href'=>'/dashboard','style'=>"background-color:#913D88","class"=>"fa fa-dashboard"),
-                    'provider'      => array('access_id'=>1,'text'=>'Dashboard',  'href'=>'/dashboard','style'=>"background-color:#913D88","class"=>"fa fa-dashboard"),
-                    'doctor'        => array('access_id'=>1,'text'=>'Dashboard',  'href'=>'/dashboard','style'=>"background-color:#913D88","class"=>"fa fa-dashboard"),
+    "company"   => array('access_id'=>1,'text'=>'COMPANY CENTER',   'href'=>'/company',     'style'=>"background-color:#C0392B","class"=>"fa fa-building-o"),
+    "member"    => array('access_id'=>1,'text'=>'MEMBER CENTER',    'href'=>'/member',      'style'=>"background-color:#446CB3","class"=>"fa fa-fa-user"),
+    "billing"   => array('access_id'=>1,'text'=>'BILLING',        'href'=>'/billing',     'style'=>"background-color:#E87E04","class"=>"fa fa-credit-card"),
+    "availment" => array('access_id'=>1,'text'=>'AVAILMENT',        'href'=>'/availment',   'style'=>"background-color:#336E7B","class"=>"fa fa-medkit"),
+    "payable"   => array('access_id'=>1,'text'=>'PAYABLE',        'href'=>'/payable',     'style'=>"background-color:#F64747","class"=>"fa fa-paypal"),
+    "reports"   => array(
+                    'first'     => array('access_id'=>1,'text'=>'AVAILMENT',            'href'=>'/reports/availment_per_month',         'style'=>"background-color:#26A65B","class"=>"fa fa-circle-o"),
+                    'second'    => array('access_id'=>1,'text'=>'COMPANY AVAILMENT',    'href'=>'/reports/company_availment',           'style'=>"background-color:#26A65B","class"=>"fa fa-circle-o"),
+                    'third'     => array('access_id'=>1,'text'=>'MONITORING',           'href'=>'/reports/availment_monitoring',        'style'=>"background-color:#26A65B","class"=>"fa fa-circle-o"),
+                    'fourth'    => array('access_id'=>1,'text'=>'PAYMENT REPORT',       'href'=>'/reports/payment_report',              'style'=>"background-color:#26A65B","class"=>"fa fa-circle-o"),
+                    'fifth'     => array('access_id'=>1,'text'=>'ENDING NUMBER',        'href'=>'/reports/ending_number_per_month',     'style'=>"background-color:#26A65B","class"=>"fa fa-circle-o"),
+                    'sixth'     => array('access_id'=>1,'text'=>'BREAKDOWN',            'href'=>'/reports/breakdown',                   'style'=>"background-color:#26A65B","class"=>"fa fa-circle-o"),
+                    'seventh'   => array('access_id'=>1,'text'=>'BREAKDOWN',            'href'=>'/reports/active_per_month',            'style'=>"background-color:#26A65B","class"=>"fa fa-circle-o"),
                 ),
    
     );
 
 ?>
-<ul class="treeview-menu">
-                            <li style="background-color:#26A65B"><a href="/reports/availment_per_month"><i class="fa fa-circle-o"></i>AVAILMENT</a></li>
-                            <li style="background-color:#26A65B"><a href="/reports/company_availment"><i class="fa fa-circle-o"></i><span style="font-size:12px;">COMPANY AVAILMENT</span></a></li>
-                            <li style="background-color:#26A65B"><a href="/reports/availment_monitoring"><i class="fa fa-circle-o"></i>MONITORING</a></li>
-                            <li style="background-color:#26A65B"><a href="/reports/payment_report"><i class="fa fa-circle-o"></i>PAYMENT REPORT</a></li>
-                            <li style="background-color:#26A65B"><a href="/reports/ending_number_per_month"><i class="fa fa-circle-o"></i>ENDING NUMBER</a></li>
-                            <li style="background-color:#26A65B"><a href="/reports/breakdown"><i class="fa fa-circle-o"></i>BREAKDOWN</a></li>
-                            
-                        </ul>
-
-
 
 @foreach($_item as $key=>$item)
 @if(array_key_exists('access_id',$item))
